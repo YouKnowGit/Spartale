cf053713279ec08229df7558599f1ed85c6dd80e
hyunkikomori <jhs880514@naver.com>
2025-06-23 19:05:31 +0900

    포션 일단 업데이트

--- Spartale/Potion.cpp
+++ Spartale/Potion.cpp
@@ -1,9 +1,20 @@
 #include "Player.h"
 #include "Potion.h"
+#include <iostream>
 
-Potion::Potion(const std::string& name, int healAmount) : name(name), healAmount(healAmount) { 
+Potion::Potion(const std::string& name, int healAmount) 
+	: name(name), healAmount(healAmount) { 
 }
 
 void Potion::use(Player& player) const {
-	
+	std::cout << "포션 사용 " << name << std::endl;
+	//player.heal(healAmount);
+}
+
+std::string Potion::getName() const {
+	return name;
+}
+
+int Potion::getHealAmount() const {
+	return healAmount;
 }
